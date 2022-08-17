8f653f37f7222d0820ce3294acb29af1cdc667f8
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-08-17 23:53:36 +0300

    adding skeleton

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react'
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>This is the count {count}
+        </p>
+        <button onClick={()=>setCount(count+1)}>Click to increment</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
