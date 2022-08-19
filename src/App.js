37b830b551804ff08b6d7ad742e926bf42022eca
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-08-19 09:32:58 +0300

    trying to create conflict

--- src/App.js
+++ src/App.js
@@ -2,22 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react'
 function App() {
-  const [count, setCount] = useState(0)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>This is the count {count}
-        </p>
-        <button onClick={()=>setCount(count+1)}>Click to increment</button>
-
-
-        <p>This is something very cool now!</p>
-        <p>I wish I knew more about git!</p>
-
-
-      </header>
-    </div>
+    <section>
+      This is a hopefully conflicting section
+    </section>
   );
 }
 
